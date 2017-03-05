28b4442374878fa9080a92a80c116f8abb403765
BIGred053 <Evan@Evans-Mac-Pro.local>
2017-03-05 12:03:45 -0500

    Added browserify, jQuery, and Mustache. Updated coffee and js files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
-var concat = require('gulp-concat');
+var concat = require('gulp-concat'),
+	browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
@@ -21,5 +22,7 @@ gulp.task('coffee', function() {
 gulp.task('js', function () {
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
+
