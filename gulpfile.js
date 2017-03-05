5fec241f96678fcd3047b0b909178ad1fb1b26be
BIGred053 <Evan@Evans-Mac-Pro.local>
2017-03-05 13:02:49 -0500

    Added watcher for json files to reload on update.

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,8 @@ var jsSources = [
 	'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -62,6 +64,8 @@ gulp.task('watch', function () {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
 });
 
 
@@ -76,10 +80,23 @@ gulp.task('connect', function() {
 	});
 });
 
+// HTML reload task
+
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+		.pipe(connect.reload());
+})
+
+// JSON reload task
+
+gulp.task('json', function() {
+	gulp.src(jsonSources)
+		.pipe(connect.reload());
+})
 
 // Run all tasks in specified sequence
 // Calling this task default, so that it runs by just typing gulp
 // 
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
 
